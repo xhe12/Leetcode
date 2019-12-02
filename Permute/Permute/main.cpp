2c9fd7502163c556b5a21922eee2c27f0a6e543f
Xuan He <XuanHe1@Xuans-MacBook-Pro.local>
2019-12-02 15:14:53 -0500

    update files

--- Permute/Permute/main.cpp
+++ Permute/Permute/main.cpp
@@ -1,13 +1,50 @@
 //
 //  main.cpp
-//  Permute
+//  Leetcode #46: Permute
 //
 //  Created by Xuan He on 12/1/19.
 //  Copyright © 2019 Xuan He. All rights reserved.
 //
 
 #include <iostream>
+#include <vector>
+using namespace std;
 
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> permuteSet;
+        vector<int> curSet;
+        vector<bool> visited(nums.size(),false);
+        
+        if (nums.empty())
+        {
+            return {};
+        }
+        findPermuteSet(nums,permuteSet,curSet,visited,0);
+        return permuteSet;
+    }
+    
+    void findPermuteSet(const vector<int>& nums,vector<vector<int>> &permuteSet,vector<int> &curSet,vector<bool> &visited,int levelIndx)
+    {
+        if(levelIndx == nums.size())
+        {
+            permuteSet.push_back(curSet);
+            return;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(!visited[i])
+            {
+                visited[i] = true;
+                curSet.push_back(nums[i]);
+                findPermuteSet(nums, permuteSet, curSet, visited, levelIndx+1);
+                curSet.pop_back();
+                visited[i] = false;
+            }
+        }
+    }
+};
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
