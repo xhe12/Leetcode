dc4a6b845f4b073c4a007f64e76563a5023ddb65
sharonhe <sharonhe@mathworks.com>
2019-12-09 10:31:12 -0500

    upload Clone Graph

--- Clone_Graph/Clone_Graph/main.cpp
+++ Clone_Graph/Clone_Graph/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 using namespace std;
 
 // Definition for a Node.
@@ -29,27 +30,46 @@ class Solution {
 public:
     Node* cloneGraph(Node* node) {
         unordered_map<Node*,Node*> nodeCorr;
+        unordered_set<Node*> processed;
         
         Node* nodeCopy = new Node(node->val,{});
-        
-        correspondNodes(node,nodeCopy,nodeCorr);
+        nodeCorr[node] = nodeCopy;
+        correspondNodes(node,nodeCopy,nodeCorr,processed);
         
         return nodeCopy;
     }
     
+    Node* cloneGraph_DFS_iterative(Node* node){
+        
+    }
+    
+    Node* cloneGraph_BFS_iterative(Node* node){
+        
+    }
+    
+    
 private:
-    void correspondNodes(Node* node,Node* nodeCopy,unordered_map<Node*,Node*> &nodeCorr)
+    void correspondNodes(Node* node,Node* nodeCopy,unordered_map<Node*,Node*> &nodeCorr,unordered_set<Node*> &processed)
     {
-        if(nodeCorr.find(node)!=nodeCorr.end())
+        if(processed.find(nodeCopy)!=processed.end())
         {
             return;
         }
         for(Node *x: node->neighbors)
         {
-            
-            Node* nodeCopy = new Node(x->val,{});
-            node->neighbors.push_back(nodeCopy);
-            correspondNodes(x, nodeCopy, nodeCorr);
+            if(nodeCorr.find(x)==nodeCorr.end())
+            {
+                Node* xCopy = new Node(x->val,{});
+                nodeCorr[x] = xCopy;
+            }
+            nodeCopy->neighbors.push_back(nodeCorr[x]);
+            //correspondNodes(x, xCopy, nodeCorr,processed);
+        }
+        processed.insert(nodeCopy);
+        
+        for(Node *x: node->neighbors)
+        {
+            correspondNodes(x, xCopy, nodeCorr,processed);
         }
     }
 };
